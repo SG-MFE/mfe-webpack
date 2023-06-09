98a527fe3a47387d0acbd8d55c833185fedad70c
Noah-DESKTOP-BGIRFLI <blade_corp@hotmail.com>
2023-06-09 12:45:37 -0600

    ok

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,13 +3,13 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.DOMAIN_NAME;
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    // publicPath: '/container/latest/',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
