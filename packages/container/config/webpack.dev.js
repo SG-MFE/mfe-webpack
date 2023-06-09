44cba0da367d8d1ea34d41ad8c5121937be08ddf
Noah-DESKTOP-BGIRFLI <blade_corp@hotmail.com>
2023-06-09 12:47:46 -0600

    e

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -4,7 +4,7 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: 'development', 
+  mode: 'development',
   devServer: {
     port: 8080,
     historyApiFallback: {
